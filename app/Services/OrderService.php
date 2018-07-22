9bd81014039b42f490c1f475f46637fa9a0f263c
wizardchao <18317119662@139.com>
2018-07-22 04:06:42 +0000

    优化优惠券模块

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -61,7 +61,7 @@ class OrderService
 
             if ($coupon) {
                 // 总金额已经计算出来了，检查是否符合优惠券规则
-                $coupon->checkAvailable($totalAmount);
+                $coupon->checkAvailable($user, $totalAmount);
                 // 把订单金额修改为优惠后的金额
                 $totalAmount = $coupon->getAdjustedPrice($totalAmount);
                 // 将订单与优惠券关联
